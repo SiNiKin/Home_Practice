64e5c3433f0c76c6161a9d86ca93d8eeebc43a00
SiNiKin <tlsdl99@gmail.com>
2024-03-13 22:39:13 +0900

    복습하자

--- FirstJava/src/quiz/ForStarDiamond.java
+++ FirstJava/src/quiz/ForStarDiamond.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ForStarDiamond {
 
 	public static void main(String[] args) {
-		// 다이아몬드 찍기
+		// 다이아몬드 찍기(0312 오전에)
 		// 1. 다음과 같은 형태로 별직기를 진행해 보세요.  
 		/*          힌트  : 공백, 별, 단(줄)
 		      *
@@ -18,70 +18,74 @@ public class ForStarDiamond {
 		     ***
 		      *
 		 */
-//		int dan = 5;
-//		
-//		System.out.println("1) ");
-//		for (int i = 0; i < dan; i++) {
-//			// space를 찍기...
-//			for (int j = 0; j < dan - i - 1; j++) {
-//				System.out.print(" ");
-//			}
-//			// 별찍기...
-//			for (int j = 0; j < i*2 + 1; j++) {
-//				System.out.print("*");
-//			}
-//			System.out.println(); 	// 줄바꿈
-//		}
-//		for (int i = 1; i < dan; i++) {
-//			// space를 찍기...
-//			for (int j = 0; j < i; j++) {
-//				System.out.print(" ");
-//			}
-//			// 별찍기...
-//			for (int j = 0; j < 2*dan - (2*i) - 1; j++) {
-//				System.out.print("*");
-//			}
-//			System.out.println(); 	// 줄바꿈
-//		}
 
+		Scanner scan = new Scanner(System.in);
+		 
+		// 줄 수 입력 받는 부분... 
+		System.out.print("줄 수 를 입력해주세요 : ");
+		int dan = scan.nextInt();
+		 
+//		for (int i = 0; i < dan; i++) {
+//			for(int j = 0; j < dan - i - 1; j++) {
+//				System.out.print(" ");
+//			}
+//			for (int j = 0; j < i * 2 + 1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//		for (int i = 0; i < dan - 1; i ++) {
+//			for(int j = 0; j < i + 1; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j = 0; j < 2 * (dan - i - 1) -1;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
 		// 2. 1번의 코드를 활용하여 줄 수를 입력 받아서 그 줄 수 에 맞는 다이아몬드 별을 찍는 
  		//   프로그램을 작성해 보세요. (단, 줄은 홀수로만 입력 받아야 합니다.)
+
+		// 변수 선언 : sp(여백), st(별), 반전을 위한 flag(boolean)- true(삼각형), false(역삼각형)
+		int sp = dan/2;
+		int st = 1;
+		boolean flag = true;
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.print(">> ");
-		int line = scan.nextInt();
-		
-		if (line % 2 == 0) {
-			System.out.println("입력값이 잘못 되었습니다.");
-		}else {
-			System.out.println("사용자가 입력한 문자열 : " + line);
+		for (int i = 0 ; i < dan ; i ++ ) {
 			
-			for (int i = 0; i < line; i++) {
-				// space를 찍기...
-				for (int j = 0; j < line - i - 1; j++) {
-					System.out.print(" ");
-				}
-				// 별찍기...
-				for (int j = 0; j < i*2 + 1; j++) {
-					System.out.print("*");
-				}
-				System.out.println(); 	// 줄바꿈
+			// 여백처리... 
+			for (int j = 0; j < sp; j++) {
+				System.out.print(" ");
 			}
-			
-			for (int i = 1; i < line; i++) {
-				// space를 찍기...
-				for (int j = 1; j < i + 1; j++) {
-					System.out.print(" ");
-				}
-				// 별찍기...
-				for (int j = 0; j < 2*line - (2*i) -1; j++) {
+			// 별찍기... 
+			for (int j = 0; j < st; j++) {
+				if(j == 0 || j == st -1)
 					System.out.print("*");
+				else {
+					if (j%2 == 0) System.out.print("$");
+					else System.out.print(" ");
 				}
-				System.out.println(); 	// 줄바꿈
 			}
+			System.out.println();
+			if(i == (dan/2)) flag = false; //반적
+			if(flag) {
+				sp -= 1; st += 2;
+			}else {
+				sp += 1; st -= 2;
+			}
+				
 		}
-
-
+		
+		scan.close();
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- FirstJava/src/quiz/Quiz_star2_240311.java
+++ FirstJava/src/quiz/Quiz_star2_240311.java
@@ -4,38 +4,44 @@ public class Quiz_star2_240311 {
 
 	public static void main(String[] args) {
 		// 피라미드 찍기
+		/*
+		 	#1   *		 #2 *********
+		        ***			 *******
+		       *****		  *****
+		      *******		   ***
+		     *********		    *
+		 */
 		
+		// #1
 		int dan = 5;
 		
-		System.out.println("1) ");
 		for (int i = 0; i < dan; i++) {
-			// space를 찍기...
-			for (int j = 0; j < dan - i - 1; j++) {
+			// space 출력
+			for (int j = 0; j < dan -i -1; j++) {
 				System.out.print(" ");
 			}
-			// 별찍기...
+			// 별 찍기
 			for (int j = 0; j < i*2 + 1; j++) {
 				System.out.print("*");
 			}
-			System.out.println(); 	// 줄바꿈
+			System.out.println();
 		}
+		
 		System.out.println();
 		
-		
-		System.out.println("2) ");
+		// #2
 		for (int i = 0; i < dan; i++) {
-			// space를 찍기...
+			// space 출력
 			for (int j = 0; j < i; j++) {
 				System.out.print(" ");
 			}
-			// 별찍기...
-			for (int j = 0; j < 2*dan - (2*i) - 1; j++) {
+			// 별 찍기
+			for (int j = 0; j < 2*dan -(2*i) - 1; j++) {
 				System.out.print("*");
 			}
-			System.out.println(); 	// 줄바꿈
+			System.out.println();
 		}
-		System.out.println();
-
+		
 	}
 
 }
